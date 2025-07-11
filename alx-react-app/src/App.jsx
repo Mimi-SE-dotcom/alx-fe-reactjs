a0ad6fa5c1a0df458f48cc9c22e2509829175c58
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-07-11 17:40:27 +0100

    Updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
     return (
@@ -10,6 +12,7 @@ function App() {
             <WelcomeMessage />
             <Header />  
             <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
             <Footer />
         </div>
     );
