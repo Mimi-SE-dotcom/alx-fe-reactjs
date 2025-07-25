eec2c06a653a3817d89ddfa2773a281326a17e1e
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-07-25 16:38:20 +0100

    set recipe

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,4 +5,29 @@ export const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
+  import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  // Add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Set all recipes
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Update an existing recipe
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  // Delete a recipe
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
