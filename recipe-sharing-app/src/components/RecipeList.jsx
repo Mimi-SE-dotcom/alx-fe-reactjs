d94310e75cfe32db37df294921e77c533cce107a
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-07-25 16:32:11 +0100

    updated react files

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useRecipeStore } from '../stores/recipeStore';
+import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      <h2>Recipe List</h2>
-      {recipes.length === 0 && <p>No recipes yet.</p>}
       {recipes.map((recipe) => (
-        <div key={recipe.id} style={{ marginBottom: '1rem' }}>
-          <h3>{recipe.title}</h3>
+        <div key={recipe.id}>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
           <p>{recipe.description}</p>
         </div>
       ))}
