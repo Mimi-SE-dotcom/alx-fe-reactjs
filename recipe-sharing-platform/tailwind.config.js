6836553588b590d137ae470cf7e98c2d2c62e64a
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-08-14 18:59:47 +0100

    Updated tailwind.Config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
