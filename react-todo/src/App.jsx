ac9aa7760f703dc30211d8bde466d7e64a9c5e8c
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-08-26 16:33:22 +0100

    fixing bug

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,7 +1,14 @@
-import TodoList from "./components/TodoList";
+// src/App.jsx
+
+import React from 'react';
+import TodoList from './components/TodoList';  // Importing the TodoList component
 
 function App() {
-  return <TodoList />;
+  return (
+    <div className="App">
+      <TodoList />  {/* Render TodoList component */}
+    </div>
+  );
 }
 
 export default App;
